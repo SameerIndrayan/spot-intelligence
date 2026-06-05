edbd19bd95d037ef7cd0c0e7199ff65b89a9398d
SameerIndrayan <sameer.indrayan07@gmail.com>
2026-06-05 12:07:05 +0530

    init: spring boot  setup

---
+++ src/main/java/com/sedai/spot/SpotIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.sedai.spot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotIntelligenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotIntelligenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sedai/spot/SpotIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sedai.spot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotIntelligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
